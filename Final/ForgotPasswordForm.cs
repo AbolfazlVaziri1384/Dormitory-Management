a7e60784fa6f8748deb3dff5333315ddef91cf10
Mobina <mobina.sh2006@gmail.com>
2025-05-26 22:31:28 +0330

    Add login and forgot password forms UI

---
+++ Final/ForgotPasswordForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final
+{
+    public partial class ForgotPasswordForm : Form
+    {
+        public ForgotPasswordForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerify_Click(object sender, EventArgs e)
+        {
+            string studentId = txtStudentId.Text.Trim();
+            string nationalCode = txtNationalCode.Text.Trim();
+
+            if (studentId == "" && nationalCode == "")
+            {
+                MessageBox.Show("اطلاعات تأیید شد");
+
+                txtStudentId.Enabled = false;
+                txtNationalCode.Enabled = false;
+                btnVerify.Enabled = false;
+
+                txtNewPassword.Enabled = true;
+                txtConfirmPassword.Enabled = true;
+                btnSubmit.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("اطلاعات وارد شده نادرست است");
+            }
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (txtNewPassword.Text == txtConfirmPassword.Text && txtNewPassword.Text.Length >= 4)
+            {
+                MessageBox.Show("رمز جدید با موفقیت ثبت شد");
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("رمزها با هم مطابقت ندارند");
+            }
+        }
+
+        private void chkShowPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            bool show = chkShowPassword.Checked;
+
+            txtNewPassword.UseSystemPasswordChar = !show;
+            txtConfirmPassword.UseSystemPasswordChar = !show;
+        }
+    }
+}
---
+++ Final/LoginForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+
+            this.BackColor = ColorTranslator.FromHtml("#ECF0F1");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkShowPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            txtPassword.UseSystemPasswordChar = !chkShowPassword.Checked;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ForgotPasswordForm forgotForm = new ForgotPasswordForm();
+            var result = forgotForm.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                MessageBox.Show("اکنون می‌توانید با رمز جدید وارد شوید", "بازگشت به ورود", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtPassword.Focus();
+            }
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
