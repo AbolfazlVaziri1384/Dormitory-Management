73872a12eb977bf756228aaf308c1ffadaa82f75
AbolfazlVaziri1384 <Abolfazlvaziri006@gmail.com>
2025-07-21 11:38:59 +0330

    Develop frmSetRoomAssignment Code

--- Final/frmSetRoomAssignment.Designer.cs
+++ Final/frmSetRoomAssignment.Designer.cs
@@ -32,31 +32,44 @@
             txtSearch = new TextBox();
             pictureBox1 = new PictureBox();
             btnSave = new Button();
+            Id = new DataGridViewTextBoxColumn();
+            FirstName = new DataGridViewTextBoxColumn();
+            LastName = new DataGridViewTextBoxColumn();
+            Gender = new DataGridViewTextBoxColumn();
+            StuPerCode = new DataGridViewTextBoxColumn();
+            NationalCode = new DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)dgvStudents).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // dgvStudents
             // 
+            dgvStudents.AllowUserToAddRows = false;
+            dgvStudents.AllowUserToDeleteRows = false;
+            dgvStudents.BackgroundColor = Color.AliceBlue;
             dgvStudents.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvStudents.Location = new Point(158, 54);
+            dgvStudents.Columns.AddRange(new DataGridViewColumn[] { Id, FirstName, LastName, Gender, StuPerCode, NationalCode });
+            dgvStudents.Location = new Point(34, 30);
             dgvStudents.Name = "dgvStudents";
+            dgvStudents.ReadOnly = true;
+            dgvStudents.RightToLeft = RightToLeft.Yes;
             dgvStudents.RowHeadersWidth = 51;
-            dgvStudents.Size = new Size(458, 107);
+            dgvStudents.Size = new Size(732, 202);
             dgvStudents.TabIndex = 0;
             // 
             // txtSearch
             // 
-            txtSearch.Location = new Point(280, 208);
+            txtSearch.Location = new Point(282, 265);
             txtSearch.Multiline = true;
             txtSearch.Name = "txtSearch";
             txtSearch.Size = new Size(222, 39);
             txtSearch.TabIndex = 1;
+            txtSearch.TextChanged += txtSearch_TextChanged;
             // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.search;
-            pictureBox1.Location = new Point(508, 208);
+            pictureBox1.Location = new Point(510, 265);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(39, 39);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
@@ -68,12 +81,67 @@
             btnSave.BackColor = Color.Honeydew;
             btnSave.Font = new Font("B Koodak", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 178);
             btnSave.ForeColor = Color.MediumSeaGreen;
-            btnSave.Location = new Point(317, 336);
+            btnSave.Location = new Point(319, 338);
             btnSave.Name = "btnSave";
             btnSave.Size = new Size(154, 44);
             btnSave.TabIndex = 17;
             btnSave.Text = "ذخیره اطلاعات";
             btnSave.UseVisualStyleBackColor = false;
+            btnSave.Click += btnSave_Click;
+            // 
+            // Id
+            // 
+            Id.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            Id.HeaderText = "Id";
+            Id.MinimumWidth = 6;
+            Id.Name = "Id";
+            Id.ReadOnly = true;
+            Id.Visible = false;
+            Id.Width = 51;
+            // 
+            // FirstName
+            // 
+            FirstName.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            FirstName.HeaderText = "نام";
+            FirstName.MinimumWidth = 6;
+            FirstName.Name = "FirstName";
+            FirstName.ReadOnly = true;
+            FirstName.Width = 56;
+            // 
+            // LastName
+            // 
+            LastName.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            LastName.HeaderText = "نام خانوادگی";
+            LastName.MinimumWidth = 6;
+            LastName.Name = "LastName";
+            LastName.ReadOnly = true;
+            LastName.Width = 119;
+            // 
+            // Gender
+            // 
+            Gender.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            Gender.HeaderText = "جنسیت";
+            Gender.MinimumWidth = 6;
+            Gender.Name = "Gender";
+            Gender.ReadOnly = true;
+            Gender.Width = 84;
+            // 
+            // StuPerCode
+            // 
+            StuPerCode.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            StuPerCode.HeaderText = " شماره دانشجویی/پرسنلی";
+            StuPerCode.MinimumWidth = 6;
+            StuPerCode.Name = "StuPerCode";
+            StuPerCode.ReadOnly = true;
+            StuPerCode.Width = 185;
+            // 
+            // NationalCode
+            // 
+            NationalCode.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            NationalCode.HeaderText = "شماره ملی";
+            NationalCode.MinimumWidth = 6;
+            NationalCode.Name = "NationalCode";
+            NationalCode.ReadOnly = true;
             // 
             // frmSetRoomAssignment
             // 
@@ -84,8 +152,11 @@
             Controls.Add(pictureBox1);
             Controls.Add(txtSearch);
             Controls.Add(dgvStudents);
+            MaximizeBox = false;
             Name = "frmSetRoomAssignment";
+            StartPosition = FormStartPosition.CenterParent;
             Text = "تخصیص اتاق";
+            Load += frmSetRoomAssignment_Load;
             ((System.ComponentModel.ISupportInitialize)dgvStudents).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
@@ -98,5 +169,11 @@
         private TextBox txtSearch;
         private PictureBox pictureBox1;
         private Button btnSave;
+        private DataGridViewTextBoxColumn Id;
+        private DataGridViewTextBoxColumn FirstName;
+        private DataGridViewTextBoxColumn LastName;
+        private DataGridViewTextBoxColumn Gender;
+        private DataGridViewTextBoxColumn StuPerCode;
+        private DataGridViewTextBoxColumn NationalCode;
     }
 }
--- Final/frmSetRoomAssignment.cs
+++ Final/frmSetRoomAssignment.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Final.Models;
+using Final.Tools;
 
 namespace Final
 {
@@ -15,6 +17,96 @@ namespace Final
         public frmSetRoomAssignment()
         {
             InitializeComponent();
+        }
+        DormitoryDbContext db;
+        public long UserID;
+        public long RoomId;
+        public long RoomAssigmentEditId = -1;
+        private void frmSetRoomAssignment_Load(object sender, EventArgs e)
+        {
+            if (RoomAssigmentEditId != -1)
+            {
+                btnSave.Text = "ویرایش";
+                frmSetRoomAssignment frm = new frmSetRoomAssignment();
+                frm.Text = "ویرایش تخصیص اتاق";
+            }
+            db = new DormitoryDbContext();
+            RefreshStudentsList(db.Users.ToList());
+            db.Dispose();
+        }
+        private void RefreshStudentsList(List<Models.User> Userslist)
+        {
+            dgvStudents.Rows.Clear();
+            foreach (var item in Userslist)
+            {
+                //نباید مسئول خاصی باشه
+                //ادمین - مدیر - مسئول خوابگاه
+                if ((Models.Role.FindRole(item.Id) != (int)EnumTool.Role.Admin) || (Models.Role.FindRole(item.Id) != (int)EnumTool.Role.Manager) || (Models.Role.FindRole(item.Id) != (int)EnumTool.Role.DormitoryOwner))
+                    //نباید قبلا بهش اتاقی داده باشیم
+                    if ((!RoomAssigment.AnyRoomAssigment(item.Id)) && (item.IsDeleted == false))
+                        //باید جنسیت با خوابگاه یکی باشه
+                        if ((Dormitory.FindDormitoryType(RoomId) == item.Gender) || (Dormitory.FindDormitoryType(RoomId) == 2))
+                        {
+                            dgvStudents.Rows.Add(item.Id.ToString(),
+                                              item.FirstName,
+                                              item.LastName,
+                                             (item.Gender == 0) ? "خانم" : "آقا",
+                                              item.StuPerCode,
+                                              item.NationalCode
+                                              );
+                        }
+            }
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                db = new DormitoryDbContext();
+                RefreshStudentsList((List<Models.User>)db.Users.Where(i => i.FirstName.Contains(txtSearch.Text.Trim()) ||
+                                                   i.LastName.Contains(txtSearch.Text.Trim()) ||
+                                                   i.StuPerCode.ToString().Contains(txtSearch.Text.Trim()) ||
+                                                   i.NationalCode.ToString().Contains(txtSearch.Text.Trim())).ToList());
+                db.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBoxTool.msger(ex.ToString());
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dgvStudents.Rows.Count == 0)
+                {
+                    return;
+                }
+                long id;
+                id = int.Parse(dgvStudents.CurrentRow.Cells[0].Value.ToString());
+                if (RoomAssigmentEditId == -1)
+                {
+                    RoomAssigment.SetRoomAssigment(id, RoomId, UserID);
+                    MessageBoxTool.msgr("تخصیص اتاف با موفقیت انجام شد");
+                    Close();
+                }
+                else
+                {
+                    DialogResult result;
+                    result = MessageBoxTool.msgq("آیا از ویرایش مطمئن هستید؟");
+                    if (result == DialogResult.Yes)
+                    {
+                        RoomAssigment.EditRoomAssigment(RoomAssigmentEditId, UserID, id);
+                        MessageBoxTool.msgr("تخصیص اتاق با موفقیت ویرایش شد");
+                        Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBoxTool.msger(ex.ToString());
+            }
         }
     }
 }
