237e17ef6b986cecddaa450480dbe504b6b99ec3
AbolfazlVaziri1384 <Abolfazlvaziri006@gmail.com>
2025-07-21 10:31:44 +0330

    Develop frmRoomAssignment Code

--- Final/frmRoomAssignment.Designer.cs
+++ Final/frmRoomAssignment.Designer.cs
@@ -29,43 +29,53 @@
         private void InitializeComponent()
         {
             dgvStudents = new DataGridView();
-            lblRoomInfo = new Label();
             pictureBox6 = new PictureBox();
             btnPrint = new Button();
             pictureBox2 = new PictureBox();
             btnDelete = new Button();
             pictureBox5 = new PictureBox();
             btnSetRoomAssignment = new Button();
+            groupBox1 = new GroupBox();
+            lblDormitoryOwner = new Label();
+            lblBlockOwner = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            lblBlock = new Label();
+            label1 = new Label();
+            lblDormitory = new Label();
+            label6 = new Label();
+            stiRoomAssigmentPrint = new Stimulsoft.Report.StiReport();
+            Id = new DataGridViewTextBoxColumn();
+            Name = new DataGridViewTextBoxColumn();
+            StuPerCode = new DataGridViewTextBoxColumn();
+            NationalCode = new DataGridViewTextBoxColumn();
+            CreateOn = new DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)dgvStudents).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // dgvStudents
             // 
+            dgvStudents.AllowUserToAddRows = false;
+            dgvStudents.AllowUserToDeleteRows = false;
+            dgvStudents.BackgroundColor = Color.AliceBlue;
             dgvStudents.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvStudents.Location = new Point(156, 56);
+            dgvStudents.Columns.AddRange(new DataGridViewColumn[] { Id, Name, StuPerCode, NationalCode, CreateOn });
+            dgvStudents.Location = new Point(48, 195);
             dgvStudents.Name = "dgvStudents";
+            dgvStudents.ReadOnly = true;
+            dgvStudents.RightToLeft = RightToLeft.Yes;
             dgvStudents.RowHeadersWidth = 51;
-            dgvStudents.Size = new Size(483, 106);
+            dgvStudents.Size = new Size(613, 161);
             dgvStudents.TabIndex = 0;
-            // 
-            // lblRoomInfo
-            // 
-            lblRoomInfo.AutoSize = true;
-            lblRoomInfo.Font = new Font("B Koodak", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 178);
-            lblRoomInfo.ForeColor = Color.RoyalBlue;
-            lblRoomInfo.Location = new Point(645, 99);
-            lblRoomInfo.Name = "lblRoomInfo";
-            lblRoomInfo.Size = new Size(95, 31);
-            lblRoomInfo.TabIndex = 1;
-            lblRoomInfo.Text = ":اطلاعات اتاق";
             // 
             // pictureBox6
             // 
             pictureBox6.Image = Properties.Resources.pl;
-            pictureBox6.Location = new Point(611, 327);
+            pictureBox6.Location = new Point(571, 378);
             pictureBox6.Name = "pictureBox6";
             pictureBox6.Size = new Size(43, 38);
             pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
@@ -77,17 +87,18 @@
             btnPrint.BackColor = Color.AliceBlue;
             btnPrint.Font = new Font("B Koodak", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 178);
             btnPrint.ForeColor = Color.DarkOrchid;
-            btnPrint.Location = new Point(322, 327);
+            btnPrint.Location = new Point(282, 378);
             btnPrint.Name = "btnPrint";
             btnPrint.Size = new Size(104, 38);
             btnPrint.TabIndex = 32;
             btnPrint.Text = "چاپ";
             btnPrint.UseVisualStyleBackColor = false;
+            btnPrint.Click += btnPrint_Click;
             // 
             // pictureBox2
             // 
             pictureBox2.Image = Properties.Resources.printer;
-            pictureBox2.Location = new Point(433, 327);
+            pictureBox2.Location = new Point(393, 378);
             pictureBox2.Name = "pictureBox2";
             pictureBox2.Size = new Size(36, 38);
             pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
@@ -99,7 +110,7 @@
             btnDelete.BackColor = Color.AliceBlue;
             btnDelete.Font = new Font("B Koodak", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 178);
             btnDelete.ForeColor = Color.Crimson;
-            btnDelete.Location = new Point(156, 327);
+            btnDelete.Location = new Point(116, 378);
             btnDelete.Name = "btnDelete";
             btnDelete.Size = new Size(104, 38);
             btnDelete.TabIndex = 34;
@@ -109,7 +120,7 @@
             // pictureBox5
             // 
             pictureBox5.Image = Properties.Resources.trash;
-            pictureBox5.Location = new Point(260, 327);
+            pictureBox5.Location = new Point(220, 378);
             pictureBox5.Name = "pictureBox5";
             pictureBox5.Size = new Size(43, 38);
             pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
@@ -121,45 +132,243 @@
             btnSetRoomAssignment.BackColor = Color.AliceBlue;
             btnSetRoomAssignment.Font = new Font("B Koodak", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 178);
             btnSetRoomAssignment.ForeColor = Color.ForestGreen;
-            btnSetRoomAssignment.Location = new Point(488, 327);
+            btnSetRoomAssignment.Location = new Point(448, 378);
             btnSetRoomAssignment.Name = "btnSetRoomAssignment";
             btnSetRoomAssignment.Size = new Size(117, 38);
             btnSetRoomAssignment.TabIndex = 35;
             btnSetRoomAssignment.Text = "تخصیص جدید";
             btnSetRoomAssignment.UseVisualStyleBackColor = false;
             // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(lblDormitoryOwner);
+            groupBox1.Controls.Add(lblBlockOwner);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(lblBlock);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(lblDormitory);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Font = new Font("B Koodak", 12F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            groupBox1.ForeColor = SystemColors.HotTrack;
+            groupBox1.Location = new Point(48, 22);
+            groupBox1.Name = "groupBox1";
+            groupBox1.RightToLeft = RightToLeft.Yes;
+            groupBox1.Size = new Size(613, 167);
+            groupBox1.TabIndex = 36;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "اطلاعات اتاق :";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // lblDormitoryOwner
+            // 
+            lblDormitoryOwner.AutoSize = true;
+            lblDormitoryOwner.Font = new Font("B Koodak", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            lblDormitoryOwner.ForeColor = Color.OliveDrab;
+            lblDormitoryOwner.Location = new Point(313, 101);
+            lblDormitoryOwner.Name = "lblDormitoryOwner";
+            lblDormitoryOwner.Size = new Size(165, 31);
+            lblDormitoryOwner.TabIndex = 44;
+            lblDormitoryOwner.Text = "DormitoryOwner";
+            // 
+            // lblBlockOwner
+            // 
+            lblBlockOwner.AutoSize = true;
+            lblBlockOwner.Font = new Font("B Koodak", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            lblBlockOwner.ForeColor = Color.SaddleBrown;
+            lblBlockOwner.Location = new Point(45, 103);
+            lblBlockOwner.Name = "lblBlockOwner";
+            lblBlockOwner.Size = new Size(127, 31);
+            lblBlockOwner.TabIndex = 40;
+            lblBlockOwner.Text = "BlockOwner";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("B Koodak", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            label3.ForeColor = Color.SaddleBrown;
+            label3.Location = new Point(180, 102);
+            label3.Name = "label3";
+            label3.Size = new Size(100, 31);
+            label3.TabIndex = 39;
+            label3.Text = "مسئول بلوک :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("B Koodak", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            label4.ForeColor = Color.OliveDrab;
+            label4.Location = new Point(490, 101);
+            label4.Name = "label4";
+            label4.Size = new Size(112, 31);
+            label4.TabIndex = 43;
+            label4.Text = "مسئول خوابگاه :";
+            // 
+            // lblBlock
+            // 
+            lblBlock.AutoSize = true;
+            lblBlock.Font = new Font("B Koodak", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            lblBlock.ForeColor = Color.SaddleBrown;
+            lblBlock.Location = new Point(46, 52);
+            lblBlock.Name = "lblBlock";
+            lblBlock.Size = new Size(120, 31);
+            lblBlock.TabIndex = 38;
+            lblBlock.Text = "BlockName";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("B Koodak", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            label1.ForeColor = Color.SaddleBrown;
+            label1.Location = new Point(204, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(76, 31);
+            label1.TabIndex = 37;
+            label1.Text = "نام بلوک :";
+            // 
+            // lblDormitory
+            // 
+            lblDormitory.AutoSize = true;
+            lblDormitory.Font = new Font("B Koodak", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            lblDormitory.ForeColor = Color.OliveDrab;
+            lblDormitory.Location = new Point(315, 48);
+            lblDormitory.Name = "lblDormitory";
+            lblDormitory.Size = new Size(158, 31);
+            lblDormitory.TabIndex = 42;
+            lblDormitory.Text = "DormitoryName";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("B Koodak", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            label6.ForeColor = Color.OliveDrab;
+            label6.Location = new Point(517, 48);
+            label6.Name = "label6";
+            label6.Size = new Size(85, 31);
+            label6.TabIndex = 41;
+            label6.Text = "نام خوبگاه :";
+            // 
+            // stiRoomAssigmentPrint
+            // 
+            stiRoomAssigmentPrint.CookieContainer = null;
+            stiRoomAssigmentPrint.EngineVersion = Stimulsoft.Report.Engine.StiEngineVersion.EngineV2;
+            stiRoomAssigmentPrint.HttpHeadersContainer = null;
+            stiRoomAssigmentPrint.Key = "08ea257179d44bf7ba1eb99220408389";
+            stiRoomAssigmentPrint.ReferencedAssemblies = new string[]
+    {
+    "System.Dll",
+    "System.Drawing.Dll",
+    "System.Windows.Forms.Dll",
+    "System.Data.Dll",
+    "System.Xml.Dll",
+    "Stimulsoft.Controls.Dll",
+    "Stimulsoft.Base.Dll",
+    "Stimulsoft.Report.Dll"
+    };
+            stiRoomAssigmentPrint.ReportAlias = "Report";
+            stiRoomAssigmentPrint.ReportGuid = "69f560883654496496bcfd77328c8026";
+            stiRoomAssigmentPrint.ReportName = "Report";
+            stiRoomAssigmentPrint.ReportSource = null;
+            stiRoomAssigmentPrint.ReportUnit = Stimulsoft.Report.StiReportUnitType.Centimeters;
+            stiRoomAssigmentPrint.ScriptLanguage = Stimulsoft.Report.StiReportLanguageType.CSharp;
+            stiRoomAssigmentPrint.UseProgressInThread = false;
+            // 
+            // Id
+            // 
+            Id.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            Id.HeaderText = "Id";
+            Id.MinimumWidth = 6;
+            Id.Name = "Id";
+            Id.ReadOnly = true;
+            Id.Visible = false;
+            Id.Width = 51;
+            // 
+            // Name
+            // 
+            Name.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            Name.HeaderText = "نام";
+            Name.MinimumWidth = 6;
+            Name.Name = "Name";
+            Name.ReadOnly = true;
+            Name.Width = 56;
+            // 
+            // StuPerCode
+            // 
+            StuPerCode.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            StuPerCode.HeaderText = "شماره دانشجویی";
+            StuPerCode.MinimumWidth = 6;
+            StuPerCode.Name = "StuPerCode";
+            StuPerCode.ReadOnly = true;
+            StuPerCode.Width = 147;
+            // 
+            // NationalCode
+            // 
+            NationalCode.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            NationalCode.HeaderText = "کد ملی";
+            NationalCode.MinimumWidth = 6;
+            NationalCode.Name = "NationalCode";
+            NationalCode.ReadOnly = true;
+            NationalCode.Width = 85;
+            // 
+            // CreateOn
+            // 
+            CreateOn.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            CreateOn.HeaderText = "تاریخ ثبت";
+            CreateOn.MinimumWidth = 6;
+            CreateOn.Name = "CreateOn";
+            CreateOn.ReadOnly = true;
+            CreateOn.Width = 96;
+            // 
             // frmRoomAssignment
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(709, 450);
+            Controls.Add(groupBox1);
             Controls.Add(btnSetRoomAssignment);
             Controls.Add(btnDelete);
             Controls.Add(pictureBox5);
             Controls.Add(btnPrint);
             Controls.Add(pictureBox2);
             Controls.Add(pictureBox6);
-            Controls.Add(lblRoomInfo);
             Controls.Add(dgvStudents);
+            MaximizeBox = false;
             Name = "frmRoomAssignment";
-            Text = "frmRoomAssignment";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "RoomAssignment";
+            Load += frmRoomAssignment_Load;
             ((System.ComponentModel.ISupportInitialize)dgvStudents).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dgvStudents;
-        private Label lblRoomInfo;
         private PictureBox pictureBox6;
         private Button btnPrint;
         private PictureBox pictureBox2;
         private Button btnDelete;
         private PictureBox pictureBox5;
         private Button btnSetRoomAssignment;
+        private GroupBox groupBox1;
+        private Label label1;
+        private Label lblBlock;
+        private Label lblBlockOwner;
+        private Label label3;
+        private Label lblDormitoryOwner;
+        private Label label4;
+        private Label lblDormitory;
+        private Label label6;
+        private Stimulsoft.Report.StiReport stiRoomAssigmentPrint;
+        private DataGridViewTextBoxColumn Id;
+        private DataGridViewTextBoxColumn Name;
+        private DataGridViewTextBoxColumn StuPerCode;
+        private DataGridViewTextBoxColumn NationalCode;
+        private DataGridViewTextBoxColumn CreateOn;
     }
 }
--- Final/frmRoomAssignment.cs
+++ Final/frmRoomAssignment.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Final.Models;
+using Final.Tools;
 
 namespace Final
 {
@@ -15,6 +17,106 @@ namespace Final
         public frmRoomAssignment()
         {
             InitializeComponent();
+        }
+        public long UserID;
+        public long RoomId;
+        DormitoryDbContext db;
+        private void frmRoomAssignment_Load(object sender, EventArgs e)
+        {
+            frmRoomAssignment frmRoomAssigment = new frmRoomAssignment();
+
+            Models.Room? room = Models.Room.FindRoomById(RoomId);
+            Models.Block? block = Models.Block.FindBlockById(room.BlockId);
+            Models.Dormitory? dormitory = Models.Dormitory.FindDormitoryById(block.DermitoryId);
+            frmRoomAssigment.Text = string.Format("{1} افراد اتاق شماره {0} در طبقه", room.Number, room.FloorNumber);
+            lblBlock.Text = block.Name;
+            lblDormitory.Text = dormitory.Name;
+
+            Models.User? BlockOwner = Models.Role.FindBlockOwner(room.BlockId);
+            Models.User? DormitoryOwner = Models.Role.FindBlockOwner(Models.Block.FindBlockById(room.BlockId).DermitoryId);
+
+            if (BlockOwner != null)
+            {
+                string name = User.GetFullName(BlockOwner);
+                lblBlockOwner.Text = name;
+            }
+            else lblBlockOwner.Text = "-";
+
+            if (DormitoryOwner != null)
+            {
+                string name = User.GetFullName(DormitoryOwner);
+                lblDormitoryOwner.Text = name;
+            }
+            else lblDormitoryOwner.Text = "-";
+
+            db = new DormitoryDbContext();
+            RefreshRoomAssigmentList(db.RoomAssigments.ToList());
+            db.Dispose();
+        }
+        private void RefreshRoomAssigmentList(List<Models.RoomAssigment> RoomAssigmentlist)
+        {
+            dgvStudents.Rows.Clear();
+
+            foreach (var item in RoomAssigmentlist)
+            {
+                // افراد داخل اتاق رامیاره
+
+                if ((item.RoomId == RoomId) && (item.IsDeleted == false))
+                {
+                    Models.User user = User.FindUserById(item.Id);
+                    string name = User.GetFullName(user);
+
+                    dgvStudents.Rows.Add(item.Id.ToString(),
+                                            name,
+                                            user.StuPerCode,
+                                            user.NationalCode,
+                                            HDateTimeTool.ToHDateTime(item.CreatOn));
+                }
+
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            DataTable dtRoomAssigment = new DataTable();
+            dtRoomAssigment.Columns.Add("Name");
+            dtRoomAssigment.Columns.Add("StuPerStudent");
+            dtRoomAssigment.Columns.Add("NationalCode");
+            dtRoomAssigment.Columns.Add("CreatOn");
+
+            dtRoomAssigment.Columns.Add("Dormitory");
+            dtRoomAssigment.Columns.Add("Block");
+            dtRoomAssigment.Columns.Add("DormitoryOwner");
+            dtRoomAssigment.Columns.Add("BlockOwner");
+
+            dtRoomAssigment.Columns.Add("Number");
+            dtRoomAssigment.Columns.Add("FloorNumber");
+
+            Models.Room? room = Models.Room.FindRoomById(RoomId);
+
+            foreach (DataGridViewRow row in dgvStudents.Rows)
+            {
+                dtRoomAssigment.Rows.Add(
+                    row.Cells[1].Value.ToString(),
+                    row.Cells[2].Value.ToString(),
+                    row.Cells[3].Value.ToString(),
+                    row.Cells[4].Value.ToString(),
+                    lblDormitory.Text,
+                    lblBlock.Text,
+                    lblDormitoryOwner.Text,
+                    lblBlockOwner.Text,
+                    room.Number,
+                    room.FloorNumber
+                    );
+            }
+            stiRoomAssigmentPrint.Load(Application.StartupPath + "/RoomAssigmentReport.mrt");
+            stiRoomAssigmentPrint.RegData("DTRoomAssigment", dtRoomAssigment);
+            stiRoomAssigmentPrint.Show();
         }
     }
 }
