ac6a06f0abc6318ac9dac24c0385e5136296f166
AbolfazlVaziri1384 <Abolfazlvaziri006@gmail.com>
2025-07-21 14:10:13 +0330

    Develop frmSetTransferStudentAssets Code

--- Final/frmSetTransferStudentAssets.Designer.cs
+++ Final/frmSetTransferStudentAssets.Designer.cs
@@ -32,31 +32,43 @@
             txtSearch = new TextBox();
             pictureBox1 = new PictureBox();
             btnSave = new Button();
+            Id = new DataGridViewTextBoxColumn();
+            FirstName = new DataGridViewTextBoxColumn();
+            LastName = new DataGridViewTextBoxColumn();
+            Gender = new DataGridViewTextBoxColumn();
+            StuPerCode = new DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)dgvStudents).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // dgvStudents
             // 
+            dgvStudents.AllowUserToAddRows = false;
+            dgvStudents.AllowUserToDeleteRows = false;
+            dgvStudents.BackgroundColor = Color.AliceBlue;
             dgvStudents.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvStudents.Location = new Point(117, 50);
+            dgvStudents.Columns.AddRange(new DataGridViewColumn[] { Id, FirstName, LastName, Gender, StuPerCode });
+            dgvStudents.Location = new Point(12, 12);
             dgvStudents.Name = "dgvStudents";
+            dgvStudents.ReadOnly = true;
+            dgvStudents.RightToLeft = RightToLeft.Yes;
             dgvStudents.RowHeadersWidth = 51;
-            dgvStudents.Size = new Size(567, 116);
+            dgvStudents.Size = new Size(569, 221);
             dgvStudents.TabIndex = 0;
             // 
             // txtSearch
             // 
-            txtSearch.Location = new Point(294, 225);
+            txtSearch.Location = new Point(182, 256);
             txtSearch.Multiline = true;
             txtSearch.Name = "txtSearch";
             txtSearch.Size = new Size(229, 39);
             txtSearch.TabIndex = 4;
+            txtSearch.TextChanged += txtSearch_TextChanged;
             // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.search;
-            pictureBox1.Location = new Point(522, 225);
+            pictureBox1.Location = new Point(416, 256);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(38, 39);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
@@ -68,24 +80,74 @@
             btnSave.BackColor = Color.Honeydew;
             btnSave.Font = new Font("B Koodak", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 178);
             btnSave.ForeColor = Color.DarkGreen;
-            btnSave.Location = new Point(336, 322);
+            btnSave.Location = new Point(216, 321);
             btnSave.Name = "btnSave";
             btnSave.Size = new Size(161, 41);
             btnSave.TabIndex = 20;
             btnSave.Text = "ذخیره";
             btnSave.UseVisualStyleBackColor = false;
+            btnSave.Click += btnSave_Click;
+            // 
+            // Id
+            // 
+            Id.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            Id.HeaderText = "Id";
+            Id.MinimumWidth = 6;
+            Id.Name = "Id";
+            Id.ReadOnly = true;
+            Id.Visible = false;
+            Id.Width = 51;
+            // 
+            // FirstName
+            // 
+            FirstName.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            FirstName.HeaderText = "نام";
+            FirstName.MinimumWidth = 6;
+            FirstName.Name = "FirstName";
+            FirstName.ReadOnly = true;
+            FirstName.Width = 56;
+            // 
+            // LastName
+            // 
+            LastName.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            LastName.HeaderText = "نام خانوادگی";
+            LastName.MinimumWidth = 6;
+            LastName.Name = "LastName";
+            LastName.ReadOnly = true;
+            LastName.Width = 119;
+            // 
+            // Gender
+            // 
+            Gender.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            Gender.HeaderText = "جنسیت";
+            Gender.MinimumWidth = 6;
+            Gender.Name = "Gender";
+            Gender.ReadOnly = true;
+            Gender.Width = 84;
+            // 
+            // StuPerCode
+            // 
+            StuPerCode.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            StuPerCode.HeaderText = "شماره دانشجویی/پرسنلی";
+            StuPerCode.MinimumWidth = 6;
+            StuPerCode.Name = "StuPerCode";
+            StuPerCode.ReadOnly = true;
+            StuPerCode.Width = 182;
             // 
             // frmSetTransferStudentAssets
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(596, 380);
             Controls.Add(btnSave);
             Controls.Add(pictureBox1);
             Controls.Add(txtSearch);
             Controls.Add(dgvStudents);
+            MaximizeBox = false;
             Name = "frmSetTransferStudentAssets";
+            StartPosition = FormStartPosition.CenterParent;
             Text = "ثبت انتقال مالکیت";
+            Load += frmSetTransferStudentAssets_Load;
             ((System.ComponentModel.ISupportInitialize)dgvStudents).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
@@ -98,5 +160,10 @@
         private TextBox txtSearch;
         private PictureBox pictureBox1;
         private Button btnSave;
+        private DataGridViewTextBoxColumn Id;
+        private DataGridViewTextBoxColumn FirstName;
+        private DataGridViewTextBoxColumn LastName;
+        private DataGridViewTextBoxColumn Gender;
+        private DataGridViewTextBoxColumn StuPerCode;
     }
 }
